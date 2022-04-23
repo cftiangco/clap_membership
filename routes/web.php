c69a71f33d65e584b7596b227bfc1fdc5b1fb5a2
Crimson Tiangco <cftaingco@gmail.com>
2022-04-23 11:01:51 +0800

    dashboard template

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistrationController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/',[RegistrationController::class,'index']);
 
 Route::get('/application',[RegistrationController::class,'index']);
 Route::post('/application',[RegistrationController::class,'store']);
+
+
+Route::get('/dashboard',[DashboardController::class,'index']);
